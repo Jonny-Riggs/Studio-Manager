0145880f758855d4e5865570c4d785a347cf005b
Jonny-Riggs <jonny.riggs615@gmail.com>
2018-09-18 13:54:39 -0500

    created the add and delete feature for gear

---
+++ src/gear/delete_gear.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Gear from './gear';
+
+export default class Delete extends Component {
+
+    deleteGear = function(event) {
+    const authKey = this.props.token
+    const id = event.target.id.split('__')[1];
+    console.log(id)
+    fetch(`http://127.0.0.1:8000/gear/${id}/`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        "Authorization": `Token ${authKey}`
+      },
+    }).then(() => {this.props.deleteGear(id)})
+  }.bind(this)
+
+  render() {
+    return (
+      <div>
+        <button className="fluid ui icon red button"
+        id={'delete__' + this.props.id}
+        onClick={this.deleteGear}
+        >
+          <i className="delete icon" />
+        </button>
+      </div>
+    );
+  }
+}
---
+++ src/gear/post_gear.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+export default class Post extends Component {
+  render() {
+    return (
+      <div className="ui form">
+        <div className="fields">
+          <div className="field">
+            <label>Brand</label>
+            <input type="text"
+            placeholder="Brand"
+            onChange={this.props.handleFieldChange}
+            id="brand"
+            />
+          </div>
+          <div className="field">
+            <label>Model</label>
+            <input type="text"
+            placeholder="Model"
+            onChange={this.props.handleFieldChange}
+            id="model"
+            />
+          </div>
+          <div className="field">
+            <label>Description</label>
+            <input type="text"
+            placeholder="Description"
+            onChange={this.props.handleFieldChange}
+            id="description"
+            />
+          </div>
+          <div className="field">
+            <label>Accessories</label>
+            <input type="text"
+            placeholder="Accessories"
+            onChange={this.props.handleFieldChange}
+            id="accessories"
+            />
+          </div>
+          <div className="field">
+            <label>Specs</label>
+            <input type="text"
+            placeholder="Specs"
+            onChange={this.props.handleFieldChange}
+            id="specs"
+            />
+          </div>
+          <div className="field">
+            <label>Image</label>
+            <input type="text"
+            placeholder="Image"
+            onChange={this.props.handleFieldChange}
+            id="image"
+            />
+          </div>
+        <button className="ui red button" value="submit" onClick={this.props.createGear}>Add Gear</button>
+        </div>
+      </div>
+    );
+  }
+}
